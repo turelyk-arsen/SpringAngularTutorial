eb7c07ec3a72000e1ffea58c1eb842edc6fcbd8f
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-11-20 18:55:51 +0100

    first commit

---
+++ src/main/java/spring/SpringAngularTutorialApplication.java
@@ -0,0 +1,13 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularTutorialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/spring/UiApplication.java
@@ -0,0 +1,23 @@
+package spring;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UiApplication {
+	
+	@CrossOrigin(origins="http://127.0.0.1:4200", allowCredentials = "true")
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello world");
+		return model;
+	}
+
+}
